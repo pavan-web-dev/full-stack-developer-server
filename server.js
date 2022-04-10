0e8c46b2486425882a6f0c89eb087c599424ba37
Pavan Choudhari <pavanwebdevcs5610@gmail.com>
2022-04-10 17:02:48 -0400

    Change to DB

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import helloController from "./controllers/hello-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 import userController from "./controllers/user-controller.js";
 import mongoose from "mongoose";
-mongoose.connect(process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev');
+mongoose.connect('mongodb+srv://root:toor@cluster0.7yrvx.mongodb.net/webdev?retryWrites=true&w=majority');
 
 const app = express();
 app.use(cors());
